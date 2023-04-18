8b28d8ad663fec2e5d597fe379a012105ae97a0d
Tren-An Chang <tchang52@ucsc.edu>
2023-04-17 21:57:48 -0700

    update: files2

--- asgn0/get_top_words.sh
+++ asgn0/get_top_words.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-# Author: Elton Chang
-# To detect and return the top K most frequent words found in a given text
-# Check if contain required argument 
+
 
 filename=$1
 k=$2
 
-clean_file=$(mktemp)
 
-stop_words="stopwords.txt"
+# get the list of stopwords
+stopwords=$(cat stopwords)
 
-tr '[:upper:]' '[:lower:]' < "$filename" | tr -d '[:punct:]' | tr -s '[:space:]' '\n' |sort|uniq|grep -vFwxf "$stop_wrods" > "$cleanfile"
+# convert all words to lowercase and remove stopwords
+words=$(tr -cs '[:alpha:]' '[\n*]' < $filename | tr '[:upper:]' '[:lower:]' | grep -v -w -F "$stopwords")
 
-result=$(cat "$clean_file" |tr '[:upper:]' '[:lower:]' | sort | uniq -c |sort -rn |awk '{if(NR<='"$k"') print $2;else if($1==last) {print $2} else {exit}}{last=$1}' |sort)
-
-echo "$result"
-rm "$clean_file"
-
-
-
-
-
-
+# count the frequency of each word and output the top K words
+echo "$words" | sort | uniq -c | sort -rn | awk '{print $2,$1}' | sort -k2rn -k1 | head -n $k | awk '{print $1}'
