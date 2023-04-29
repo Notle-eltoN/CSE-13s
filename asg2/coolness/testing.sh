f1a473792cf98caa68bb0c4d4573cd52d18bc3c7
Live session user <trenan2004@gmail.com>
2023-04-29 21:03:51 +0000

    Initial setup

---
+++ asg2/coolness/testing.sh
@@ -0,0 +1,11 @@
+# zero arguments                                                                    
+./coolness
+# one argument                                                                      
+./coolness 32
+# two arguments                                                                     
+./coolness 32.5 10
+# temp out of range                                                                 
+./coolness 55
+# wind out of range                                                                 
+./coolness 10 0
+
